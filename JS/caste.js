4e01a1b879d43161b550283c9df4941d2553afc4
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-11-28 14:02:05 +0530

    Day 1

---
+++ JS/caste.js
@@ -0,0 +1,38 @@
+// Find Scholarship
+
+const caste = prompt("Enter your caste (o, b, s, t): ");
+const fees = parseFloat(prompt("Enter your Fees: "));
+const cgpa = parseFloat(prompt("Enter your CGPA: "));
+
+let grade;
+let sh = 0;
+
+if (cgpa > 9) {
+    grade = 'A';
+} else if (cgpa > 8.5){
+    grade = 'B';
+} else if (cgpa > 8){
+    grade = 'C';
+} else if(cgpa > 7.5){
+    grade = 'D';
+} else {
+    console.log("Low CGPA");
+}
+
+if(grade == 'A' || grade == 'B' ){
+    if(caste == 'o' || caste == 'O'){
+        sh = 0;
+    } else if (caste == 'b' || caste == 'B'){
+        sh = fees * 0.25;
+    } else if (caste == 's' || caste == 'S'){
+        sh = fees * 0.50;
+    } else if (caste == 't' || caste == 'T'){
+        sh = fees * 1;
+    }
+} else {
+    console.log("You're grade is low");
+}
+
+console.log("Scholarship =", sh);
+
+
---
+++ JS/intro.js
@@ -0,0 +1,11 @@
+// Output method
+
+console.log("World"); // Debug
+
+// alert("Learn JS"); // Alerting
+
+document.write("<h2>Hello World</h2>"); // Write in Html
+
+let age = parseInt(prompt("Enter Your Age: ", 0));
+
+console.log(age + 5);
