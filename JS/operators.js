61e259a7930a5d748a975c01b73effd0966483df
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-11-30 19:02:42 +0530

    Day - 3

--- JS/operators.js
+++ JS/operators.js
@@ -4,11 +4,12 @@
 
 // let a = 6;
 // let b = 4;
-console.log("(Addition)a + b =", a + b);
-console.log("(Subtraction)a - b =", a - b);
-console.log("(Multiplication)a * b =", a * b);
-console.log("(Division)a / b =", a / b);
-console.log("(Modulus)a % b =", a % b);
+// console.log("(Addition)a + b =", a + b);
+// console.log("(Subtraction)a - b =", a - b);
+// console.log("(Multiplication)a * b =", a * b);
+// console.log("(Division)a / b =", a / b);
+// console.log("(Modulus)a % b =", a % b);
+// console.log("(Exponentiation)a ** b =", a ** b); // Exponentiation
 
 // unary 
 // (Post Increment)a++         (Post Increment)a--
@@ -18,28 +19,39 @@ console.log("(Modulus)a % b =", a % b);
 // let a = 6;
 // let b = 4;
 
-console.log("(Addition)a += b =", a += b);  //a = a + b
-console.log("(Subtraction)a -= b =", a -= b);  // a = a - b
-console.log("(Multiplication)a *= b =", a *= b);  //  a = a * b
-console.log("(Division)a /= b =", a /= b); // a = a / b
-console.log("(Modulus)a %= b =", a %= b);  // a = a % b
+// console.log("(Addition)a += b =", a += b);  //a = a + b
+// console.log("(Subtraction)a -= b =", a -= b);  // a = a - b
+// console.log("(Multiplication)a *= b =", a *= b);  //  a = a * b
+// console.log("(Division)a /= b =", a /= b); // a = a / b
+// console.log("(Modulus)a %= b =", a %= b);  // a = a % b
 
 // Comparison Operators
 // let a = 6;
 // let b = 4;
 
-console.log("(Equal to) a == b", a == b);
-console.log("(Equal to & type) a === b", a === b); //(strict equality) check datatype also if not same false
-console.log("(Not Equal) a != b", a != b);
-console.log("(Not Equal to & type)a !== b", a !== b); //(strict inequality) check datatype also if same true
+// console.log("(Equal to) a == b", a == b);
+// console.log("(Equal to & type) a === b", a === b); //(strict equality) check datatype also if not same false
+// console.log("(Not Equal) a != b", a != b);
+// console.log("(Not Equal to & type)a !== b", a !== b); //(strict inequality) check datatype also if same true
 
 // Logical Operators
-let a = 6;
-let b = 4;
-let isFalse = false;
-let isTrue = true;
+// let a = 6;
+// let b = 4;
+// let isFalse = false;
+// let isTrue = true;
 
-console.log("a > b && b < a =",a > b && b < a); // Logical And Both should be ture
-console.log("a > b || b > a =",a > b || b > a); // Logical Or anyone can be true
-console.log("!isfalse =", !isFalse); // If the operand is false, ! will return true. 
-console.log("!isTrue =", !isTrue);  // If the operand is true, ! will return false.
+// console.log("a > b && b < a =",a > b && b < a); // Logical And Both should be ture
+// console.log("a > b || b > a =",a > b || b > a); // Logical Or anyone can be true
+// console.log("!isfalse =", !isFalse); // If the operand is false, ! will return true. 
+// console.log("!isTrue =", !isTrue);  // If the operand is true, ! will return false.
+
+// Javscript string operators
+
+// let a = '2';
+
+// a += 298;
+
+// console.log(a);
+
+// console.log('2'+ 7 + 4);
+// console.log(7 + 4 + '2');
---
+++ JS/time_math_obj.js
@@ -0,0 +1,30 @@
+// ------------ Date ------------------------
+
+let currentDate = new Date;
+console.log(currentDate);
+
+// --------------- Math -------------------
+
+let m = Math;
+
+console.log(m);
+console.log("The Value of Math.E is=", Math.E);
+console.log("The Value of Math.LN10 is=", Math.LN10);
+console.log("The Value of Math.LN2 is=", Math.LN2);
+console.log("The Value of Math.LOG10E is=", Math.LOG10E);
+console.log("The Value of Math.LOG2E is=", Math.LOG2E);
+console.log("The Value of Math.PI is=", Math.PI);
+console.log("The Value of Math.SQRT1_2 is=", Math.SQRT1_2);
+console.log("The Value of Math.SQRT2 is=", Math.SQRT2);
+
+// ---------------- Objects ------------------ 
+
+const bioDate = {
+    firstName: "John",
+    lastName: "Jenson",
+    age: 34,
+    education: true,
+    salary: undefined,
+}
+
+console.log(bioDate);
