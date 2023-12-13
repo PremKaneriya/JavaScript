a2063f9daa7461c84a750e860101eea43d5d96e5
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-12-13 23:30:47 +0530

    Day 12

--- Small Projects/DentalCare/ClassDentalCare.js
+++ Small Projects/DentalCare/ClassDentalCare.js
@@ -1,7 +1,7 @@
 const rtc_s = 5, rtc_c = 5000;
-const rcc_s = 12, rcc_c = 10000;
+const rcc_s = 12, rcc_c = 12000;
 const tc_s = 1, tc_c = 500;
-const trr_s = 3, trr_c = 2000;
+const trr_s = 3, trr_c = 3000;
 
 let tr_name, tr_s = 0, tr_c = 0;
 
@@ -45,44 +45,24 @@ function appointmentDate() {
     event.preventDefault();
 
     const aptDating = document.getElementById("aptDate").value;
-    
-    let d = new Date(aptDating);
-    // Creates a new Date object using the value of aptDating.
 
-    // console.log(d.getDate(), d.getMonth(), d.getFullYear());
-    // this log stores date, month, year in d variable
+    let d = new Date(aptDating);
 
     let print = "<table border='1'> <tr> <th>Treatment</th><th>Seating</th> <th>Costing</th> </tr>";
-    // prints table
 
     for (let i = 0; i < tr_s; i++) {
-        //this loop start with 0 to selected sittings
-
         print = print + "<tr>";
-    
         if (i === 0) {
-            // checks 0 and start rowspan
-            print = print + "<td rowspan='" + tr_s + "'>";
-            print = print + tr_name;
-            //Adds the treatment name to the cell.
-            print = print + "</td>";
+            print = print + '<td rowspan='+ tr_s +'>' + tr_name + '</td>';
         }
-
-        print = print + "<td>";
-        print = print + d.toDateString();
-        // this todatestring Adds date to cell
-        print = print + "</td>";
-
-        print = print + "<td>";
-        print = print + tr_c / tr_s;
-        // calculate the cost to sitting
-        print = print + "</td>";
-
+        print = print + "<td>"  + d.toLocaleDateString() + "</td>"
+        print = print + "<td>" + tr_c / tr_s + "</td>"
         print = print + "</tr>";
 
+
         d.setDate(d.getDate() + 7);
-        // Increments the date by 7 days for the next sitting.
     }
+
 
     print = print + "</table>";
 
