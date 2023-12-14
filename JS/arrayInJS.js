243691f31076324e0a99b216a2d4ba375a0839db
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-12-14 15:12:53 +0530

    Day 13

---
+++ JS/arrayInJS.js
@@ -0,0 +1,86 @@
+// // Creating an array of fruits
+// let fruits = ['apple', 'banana', 'orange', 'grape'];
+
+// // Accessing elements by index
+// console.log(fruits[0]); // Output: 'apple'
+// console.log(fruits[2]); // Output: 'orange'
+
+// Creating an array of numbers
+// let numbers = [1, 2, 3, 4, 5];
+
+// Displaying the initial array
+// console.log('Initial Array:', numbers);
+
+// Using push to add elements to the end of the array
+// numbers.push(6, 7);
+// console.log('Array after push:', numbers);
+
+// Using splice to remove elements from the array
+// numbers.splice(2, 2); // Removes 2 elements starting from index 2
+// console.log('Array after splice:', numbers);
+
+// Checking the length of the array
+// console.log('Length of the array:', numbers.length);
+
+// Using typeof to check the type of the array
+// console.log('Type of the array:', typeof numbers);
+
+// Creating an array of numbers
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.push(1));
+// output = Array(6) [ 1, 2, 3, 4, 5, 1 ]
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.pop());
+// removes the last number
+
+// arr[2] = 47;
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.shift());
+// removes first
+
+
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers.unshift(5));
+// Add at first
+
+// -----------------
+
+// isArray
+// let arr = [1,2,3,4,5]
+
+// console.log(Array.isArray(arr));
+
+// Concat
+
+// let arr = [1,2,3,4,5]
+// let arr1 = [1,2,3,4,5]
+
+// let ans = arr.concat(arr1);
+
+// console.log(ans);
+
+// Some
+// let arr = [1,2,3,4,5]
+
+// let ans = arr.some((a) => 2)
+// console.log(ans);
+
+// find
+
+// let arr = [1,2,3,4,5]
+
+// let ans = arr.find((a) => 2)
+// console.log(ans);
+
+
+// let arr = [1,2,3,4,5]
+
+// console.log(arr.toString());
+
