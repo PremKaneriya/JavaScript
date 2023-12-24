0a8024c5cd36dd17c5d59e3c1822feec06b1a023
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-12-24 22:43:53 +0530

    Done

---
+++ Objects_&_Classes/Objects_d_Classes.js
@@ -0,0 +1,55 @@
+// const studentInfo = {
+//     fullName : "John Jacobs",
+//     marks : 44,
+//     age : 22,
+//     present : true,
+//     fun : function insideFun() {
+//         console.log("marks",this.age); // this means parent object (studentInfo)
+//     }
+// }
+
+// console.log(studentInfo);
+
+// a prototypes are objects in an object, they are by_default;
+
+// const employee = {
+//     calculateTax(){
+//         console.log("Tax = 10%");
+//     } 
+// }
+
+// const BeeNew = {
+//     salary : 50000,
+//     calculateTax(){
+//         console.log("Tax = 30%");
+//     }  
+// }
+
+// console.log(BeeNew.calculateTax);
+
+// ---------------Classes in JS------------------------
+
+// class highEndCar {
+//     constructor (brandName, price) {
+//         console.log("Creating New Object");
+//         this.brandName = brandName;
+//         this.price = price;
+//     }
+//     start () {
+//         console.log("Start");
+//     }
+//     stop () {
+//         console.log("Stop");
+//     }
+// }
+
+
+
+// let ferarri = new highEndCar("New Ferrari", 50000);
+// let Tesla = new highEndCar("New Tesla", 68000);
+
+// console.log(ferarri);
+
+// -----------------Inheritance in JS---------------------------
+
+
---
+++ Small Projects/courseSearch/courseSearch.js
@@ -0,0 +1,41 @@
+let arr = [];
+let update = null;
+
+const updateArray = (value, index) => {
+    document.getElementById("courseSelect").value = `${value}`;
+    update = index;
+}
+
+const taskDefault = () => {
+    let print = '';
+
+    print = print + '<ul>';
+
+    arr.map((v, i) => {
+        print = print + `<li><button id="uupadate" onclick="updateArray('${v}', ${i})">E</button>${v}<button id="removeBTN" onclick="remove('${i}')"> D </button>` 
+        // print = print + `<div id="printDiv"><p id="uupadate" onclick="updateArray('${v}', ${i})"><i class="fa-solid fa-pencil" style="color: #000000;"></i></p><h4>${v}</h4><p id="removeBTN" onclick="remove('${i}')"><i class="fa-solid fa-xmark fa-lg" style="color: #000000;"></i></p></div id="printDiv">`;
+    });
+    
+    print = print + '</ul>';
+
+    document.getElementById("dispCourse").innerHTML = print;
+    document.getElementById("courseSelect").value = '';
+}
+
+const remove = (index) => {
+    arr.splice(index, 1);
+    taskDefault();
+}
+
+const taskReturn = () => {
+    event.preventDefault();
+    let courseSelect = document.getElementById("courseSelect").value;
+
+    if (update !== null || update === 0) {
+        arr[update] = courseSelect;
+        update = null;
+    } else {
+        arr.push(courseSelect);
+    }
+    taskDefault();
+}
