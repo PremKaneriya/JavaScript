c9bbea43623549fb417c7103b34834883afff973
Prem Kaneriya <premkaneriyappp@gmail.com>
2023-12-26 20:57:38 +0530

    Day 20

---
+++ JS/HW_27-12-23.js
@@ -0,0 +1,30 @@
+// 19.    Checking if two arrays are equal or not.
+
+// let arr1 = [11,44,55,66,78];
+// let arr2 = [11,44,55,66,78];
+
+// let ans = arr1.every((a, index) => a === arr2[index]);
+
+// console.log(ans);
+
+// ----------------------------------------
+
+// 23.    Finding the index of the first occurrence of an element in an array.
+
+// let arr = [11,33,44,55,66,77,88,99,55];
+
+// let ans = arr.indexOf(55);
+
+// console.log("Index of the first occurrence:", ans);
+
+// -----------------------------------------
+
+// 24.    Finding the index of the last occurrence of an element in an array.
+
+// let arr = [11,33,44,55,66,77,88,99,55];
+
+// let ans = arr.lastIndexOf(55);
+
+// console.log("Index of the first occurrence:", ans);
+
+// -------------------------------------------
