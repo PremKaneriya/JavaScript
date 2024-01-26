2bf5d23d4dc1a747215164e6a085c540c72ccbff
Prem Kaneriya <premkaneriyappp@gmail.com>
2024-01-26 13:22:00 +0530

    Homework Done

---
+++ JS/stringMethod.js
@@ -0,0 +1,79 @@
+// String Method
+// Concat
+// let text1 = 'Hello'
+// let text2 = 'World'
+// let text3 = text1.concat("",text2)
+
+// console.log(text3);
+
+// includes
+// let text = 'Hello World';
+// console.log(text.includes('r'));
+
+// indexOf
+// let text = 'Hello World';
+// console.log(text.indexOf('o'));
+
+// Startwith
+// let text = 'Hello World';
+// console.log(text.startsWith('H'))
+
+//endwith
+// let text = 'hello world'
+// console.log(text.endsWith('d'));
+
+//slice
+// let text = 'Hello World';
+// console.log(text.slice(0,5));
+
+//substring
+// let text = 'Hello World';
+// console.log(text.substring(0,5))
+
+//substr
+// let text = 'Hello World';
+// console.log(text.substr(0,5))
+
+// replace
+// let text = 'Hello World';
+// console.log(text.replace('Hello','Universe'))
+
+// replaceAll
+// let text = 'Hello World';
+// console.log(text.replaceAll('Hello','Universe'))
+
+// trim
+// let text = '  Hello World  ';
+// console.log(text.trim());
+
+// charAt
+// let text = 'Hello World';
+// console.log(text.charAt(4))
+
+// charCodeAt
+// let text = 'Hello World';
+// console.log(text.charCodeAt(6))
+
+// fromCharCode
+// let text = 'Hello World';
+// console.log(String.fromCharCode(72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100))
+
+// split
+// let text = 'Hello World';
+// console.log(text.split(' '))
+
+// match
+// let text = 'Hello World';
+// console.log(text.match(/Hello/))
+
+// test
+// let text = 'Hello World';
+// console.log(text.test(/Hello/))
+
+// toLowerCase
+// let text = 'HELLO WORLD';
+// console.log(text.toLowerCase())
+
+// toUpperCase
+// let text = 'hello world';
+// console.log(text.toUpperCase())
